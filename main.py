12da42f1b0cf910f7316efe11df938d860d9e939
Pablo Nieto Arias <pablo.nieto.arias@gmail.com>
2026-05-10 21:30:01 +0200

    FIX Report Semgrep Warning Table RM host 0.0.0.0 en Flask

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from server import app
 from routes import auth, companies, companies_admin, users_admin, profile
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True, use_reloader=False)
+    app.run(debug=True, use_reloader=False)
